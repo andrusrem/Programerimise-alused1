441ff693a1931bbd4426965cb3a8f6caf43c5486
andrusrem <74862630+andrusrem@users.noreply.github.com>
2022-09-23 13:21:05 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,17 +1,14 @@
 let temp = prompt("Mis temperatuur on praegu? ")
-let result = {-1:'Liiga külm',0:"Paras temperatuur",1:'Liiga kuum',2:'Kitjuta midagi muut'}
-function checkTemp(temp, resoult) {
+
+function checkTemp(temp) {
     if (temp <= 20){
-        return result[-1]
+        return -1
     }
     else if (temp <= 40){
-        return result[0]
+        return 0
     }
     else if (temp <= 60){
-        return result[1]
+        return 1
 
-    }
-    else {
-        return result[2]
     }
 }
